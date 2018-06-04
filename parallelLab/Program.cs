a0faab11edfde9160ad7fa05dcf1c694ca5838bc
hik023 <sashken23@gmail.com>
2018-06-04 03:31:29 +0300

    'upload'

---
+++ parallelLab/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace parallelLab
+{
+    class Program
+    {
+        static int[] mas = new int[] { 3, 1, 7, 5 };
+
+        static void sortUp()
+        {
+            lock (mas)
+            {
+                for (int i = 0; i < mas.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        if (mas[i] > mas[j])
+                        {
+                            int tmp = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = tmp;
+                        }
+                    }
+                }
+            }
+        }
+
+        static void sortDown()
+        {
+            lock (mas)
+            {
+                for (int i = 0; i < mas.Length - 1; i++)
+                {
+                    for (int j = i + 1; j < mas.Length; j++)
+                    {
+                        if (mas[i] < mas[j])
+                        {
+                            int tmp = mas[i];
+                            mas[i] = mas[j];
+                            mas[j] = tmp;
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            
+            for (int el = 0; el < mas.Length; el++)
+            {
+                Console.WriteLine(mas[el]);
+            }
+            Console.WriteLine('\n');
+            Thread t1 = new Thread(sortUp);
+            t1.Start();
+            t1.Join();
+            
+            for (int el = 0; el < mas.Length; el++)
+            {
+                Console.WriteLine(mas[el]);
+            }
+            Console.WriteLine('\n');
+            Thread t2 = new Thread(sortDown);
+            t2.Start();
+            t2.Join();
+
+            for (int el = 0; el < mas.Length; el++)
+            {
+                Console.WriteLine(mas[el]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
